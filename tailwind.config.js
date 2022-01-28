fa999f5f47e0b8b5413e737c8446928a18c82ca4
Aydın Terzi <aydinterzi7@gmail.com>
2022-01-28 19:04:57 +0300

    inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode:"jit",
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+    colors:{
+      
+    },
+    spacing:{
+      '15':'3.75rem'
+    }
+    },
+  },
+  plugins: [],
+}
